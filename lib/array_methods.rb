dfccdd5f86985f86242ddc5e2745bb6167f87eac
Daniel Matney <danieljmatney@gmail.com>
2019-09-03 05:54:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,7 +7,7 @@ def find_max_value(array)
   counter = 0
   array.length.times {
     if max < array[counter]
-    max = array[counter]
+      max = array[counter]
     end
     counter += 1
   }
@@ -19,7 +19,7 @@ def find_min_value(array)
   counter = 0
   array.length.times {
     if min > array[counter]
-    min = array[counter]
+      min = array[counter]
     end
     counter += 1
   }
