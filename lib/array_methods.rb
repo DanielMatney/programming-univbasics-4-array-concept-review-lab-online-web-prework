c02e4af5609b2986fc780071c06138d6d21d6187
Daniel Matney <danieljmatney@gmail.com>
2019-09-03 05:43:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,11 +6,11 @@ def find_max_value(array)
   max = 0
   counter = 0
   array.length.times 
+    p max
     if max < array[counter]
     max = array[counter]
-    counter += 1
     end
-    p max
+    counter += 1
 end
 
 def find_min_value(array)
